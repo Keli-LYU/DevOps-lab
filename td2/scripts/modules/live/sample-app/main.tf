2ee74f452a2306e3d2c689226da607915d77d05b
keli lyu <keli.lyu@edu.esiee.fr>
2026-01-04 02:13:14 +0100

    td2ex10

--- td2/scripts/modules/live/sample-app/main.tf
+++ td2/scripts/modules/live/sample-app/main.tf
@@ -2,18 +2,25 @@ provider "aws" {
   region = "eu-west-3"
 }
 
-module "sample_app_1" {
-  source = "../../modules/ec2-instance"
-  ami_id = "YOUR_AMI_ID" # Replace with your AMI ID
-  name = "sample-app-tofu-1"
-  instance_type = "t3.micro"
-  port = 8080
+locals {
+  instances = {
+    "sample-app-tofu-1" = {
+      instance_type = "t3.micro"
+      port          = 8080
+    }
+    "sample-app-tofu-2" = {
+      instance_type = "t3.small"
+      port          = 8080
+    }
+  }
 }
 
-module "sample_app_2" {
-  source = "../../modules/ec2-instance"
-  ami_id = "YOUR_AMI_ID" # Replace with your AMI ID
-  name = "sample-app-tofu-2"
-  instance_type = "t3.micro"
-  port = 8080
+module "sample_app" {
+  source        = "../../modules/ec2-instance"
+  for_each      = local.instances
+  
+  ami_id        = "YOUR_AMI_ID" # Replace with your AMI ID
+  name          = each.key
+  instance_type = each.value.instance_type
+  port          = each.value.port
 }
